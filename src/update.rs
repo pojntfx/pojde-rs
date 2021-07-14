c2e48f6ce5e9334dc11cddd8a92a0c59e29f1fb8
Felix Pojtinger <felix@pojtinger.com>
2021-07-14 11:53:05 +0000

    feat: Add RISC-V support

--- src/update.rs
+++ src/update.rs
@@ -1,10 +1,11 @@
-use std::env::consts;
+use std::{env::consts, io::Error, io::ErrorKind};
 
 use clap::{crate_name, crate_version};
 
 static REPO_OWNER: &str = "pojntfx";
 
-pub fn update() -> Result<self_update::Status, self_update::errors::Error> {
+#[cfg(not(target_arch = "riscv64"))]
+pub fn update() -> Result<String, Error> {
     let bin_suffix = match (consts::OS, consts::ARCH) {
         ("linux", "x86_64") => "linux-x86_64",
         ("linux", "aarch64") => "linux-aarch64",
@@ -19,12 +20,26 @@ pub fn update() -> Result<self_update::Status, self_update::errors::Error> {
         ),
     };
 
-    self_update::backends::github::Update::configure()
+    let res = self_update::backends::github::Update::configure()
         .repo_owner(REPO_OWNER)
         .repo_name(crate_name!())
         .bin_name(&(crate_name!().to_owned() + bin_suffix))
         .show_download_progress(true)
         .current_version(crate_version!())
-        .build()?
-        .update()
+        .build()
+        .unwrap()
+        .update();
+
+    match res {
+        Ok(s) => Ok(s.version().to_string()),
+        Err(e) => Err(Error::new(ErrorKind::Other, e)),
+    }
+}
+
+#[cfg(target_arch = "riscv64")]
+pub fn update() -> Result<String, Error> {
+    Err(Error::new(
+        ErrorKind::Other,
+        "Self-updates are not supported on riscv64 yet.",
+    ))
 }
