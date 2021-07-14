b0bf5533eb0ad05173f198ee3b0ffeb051eac6cf
Felix Pojtinger <felix@pojtinger.com>
2021-07-14 11:25:01 +0000

    feat: Add start and stop commands

--- src/instances.rs
+++ src/instances.rs
@@ -7,11 +7,19 @@ pub struct Instances {
 }
 
 impl Instances {
+    fn get_container(self: &Self, name: &str) -> shiplift::Container<'_> {
+        self.docker.containers().get(POJDE_PREFIX.to_owned() + name)
+    }
+
+    pub async fn start(self: &Self, name: &str) -> Result<(), shiplift::Error> {
+        self.get_container(name).start().await
+    }
+
+    pub async fn stop(self: &Self, name: &str) -> Result<(), shiplift::Error> {
+        self.get_container(name).stop(None).await
+    }
+
     pub async fn restart(self: &Self, name: &str) -> Result<(), shiplift::Error> {
-        self.docker
-            .containers()
-            .get(POJDE_PREFIX.to_owned() + name)
-            .restart(None)
-            .await
+        self.get_container(name).restart(None).await
     }
 }
