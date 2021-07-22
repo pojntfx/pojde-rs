f4fd579c24a77885eefa11cdf75cd6870ace7ea0
Felix Pojtinger <felix@pojtinger.com>
2021-07-22 18:13:14 +0000

    refactor: Improve self-update portability

--- src/update.rs
+++ src/update.rs
@@ -6,23 +6,14 @@ static REPO_OWNER: &str = "pojntfx";
 
 pub fn update() -> Result<self_update::Status, self_update::errors::Error> {
     let bin_suffix = match (consts::OS, consts::ARCH) {
-        ("linux", "x86_64") => "linux-x86_64",
-        ("linux", "aarch64") => "linux-aarch64",
-        ("linux", "arm") => "linux-armv7l",
-        ("linux", "riscv64") => "linux-riscv64",
-        ("windows", "x86_64") => "exe.windows-x86_64",
-        ("macos", "x86_64") => "macos-x86_64",
-        ("macos", "aarch64") => "macos-aarch64",
-        (os, arch) => panic!(
-            "Can't upgrade, unsupported OS `{}` or architecture `{}`",
-            os, arch
-        ),
+        ("windows", arch) => format!("exe.windows-{}.exe", arch), // Windows is special
+        (os, arch) => format!("{}-{}", os, arch),
     };
 
     self_update::backends::github::Update::configure()
         .repo_owner(REPO_OWNER)
         .repo_name(crate_name!())
-        .bin_name(&(crate_name!().to_owned() + bin_suffix))
+        .bin_name(&(crate_name!().to_owned() + &bin_suffix))
         .show_download_progress(true)
         .current_version(crate_version!())
         .build()?
