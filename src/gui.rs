30f36e57bd8d7553e7f5e7f0ba61231b056d3963
Felix Pojtinger <felix@pojtinger.com>
2021-07-22 18:17:30 +0000

    feat: Port GUI to RISC-V

--- src/gui.rs
+++ src/gui.rs
@@ -1,17 +1,9 @@
-#[cfg(not(target_arch = "riscv64"))]
-use pojdectl_rs::widgets;
+use pojde_rs::widgets;
 
-#[cfg(not(target_arch = "riscv64"))]
 #[tokio::main]
 pub async fn main() {
     let app = widgets::Window::default();
     let native_options = eframe::NativeOptions::default();
 
     eframe::run_native(Box::new(app), native_options);
-}
-
-#[cfg(target_arch = "riscv64")]
-#[tokio::main]
-pub async fn main() {
-    eprintln!("pojdegui is not supported on RISC-V yet.")
 }
